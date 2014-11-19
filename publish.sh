675ea9d8fcd4150538dd486563644aab1178b606
Jonas Jongejan <jonas@halfdanj.dk>
2014-11-19 18:22:29 -0500

    ...

--- publish.sh
+++ publish.sh
@@ -7,8 +7,10 @@ git clone --branch=gh-pages https://github.com/HalfdanJ/ofDocGenerator.git ghpag
 cp -R output/* ghpages/
 (
 	cd ghpages;
-	git remote set-url origin "https://${GIT_TOKEN}@github.com/HalfdanJ/ofDocGenerator.git";
-	git push -f origin gh-pages
+	git add --all .;
+	git commit -m "Travis auto ofDoc generation";
+	git remote set-url origin "https://${GIT_TOKEN}@github.com/HalfdanJ/ofDocGenerator.git"
+	git push -fq origin gh-pages  > /dev/null 2>&1
 )
 
 echo "Publishing done"
