cd40281075bca011eb5caf7679d62769c6c7f743
Travis-CI <jonas@halfdanj.dk>
2014-11-21 02:26:11 +0000

    Travis auto ofDoc generation

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function toggleDescription(elm){
   console.log("toggle", elm);
 var e = $(elm+"_description");
-  //$(elm+"_description").toggleClass("hide")
+  $(elm).toggleClass("open")
 
-  console.log($(elm + "_description").css("height"));
 
   e.slideToggle("fast", function(){
     console.log("done")
